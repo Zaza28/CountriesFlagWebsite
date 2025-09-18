3d52d9f3b682bb938ee359eb556f63781f9e62cd
Zaza28 <zainebharzi26@gmail.com>
2025-09-18 17:42:00 +0200

    add all

---
+++ src/pages/About.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Nav from "../components/Nav";
+import Logo from "../components/Logo";
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Nav />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi
+        provident cum soluta quos quia facilis voluptates, dolor, officiis
+        exercitationem illum sunt nulla quo quis omnis autem aspernatur amet
+        veritatis sit?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+        error laborum quibusdam eius hic, quasi incidunt, impedit molestias nisi
+        aut nesciunt cupiditate unde beatae voluptatem quos eveniet fuga.
+        Commodi, suscipit.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Nav from "../components/Nav";
+import Countries from "../components/Countries";
+
+const Home = () => {
+  return (
+    <div>
+      <Logo />
+      <Nav />
+      <Countries />
+    </div>
+  );
+};
+
+export default Home;
